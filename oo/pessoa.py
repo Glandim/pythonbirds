90b855b59c6206d6cde956fa93a7593bdbfacf1e
Glandim <gabriel.landim@fatec.sp.gov.br>
2021-02-09 19:18:09 -0300

    Atributos de instância nome e idade criados

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+  def __init__(self, nome=None, idade=20):
+    self.nome = nome
+    self.idade = idade
+
   def cumprimentar(self):
     return f'olá {id(self)}'
 
 if __name__ == '__main__':
-  p = Pessoa()
+  p = Pessoa('Gabriel')
   print(Pessoa.cumprimentar(p))
   print(id(p))
   print(p.cumprimentar())
-  
+  print(p.nome)
+  p.nome = 'Landim'
+  print(p.nome)
+  print(p.idade)
